75cc3ce0325a260af55cc9650e2448ca7e5b819d
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-07-11 05:47:05 -0700

    Setup Router in App.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ErrorPage from './pages/ErrorPage';
 import Header from './components/main/Header';
 import Footer from './components/main/Footer';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
-      <main>
-        <Home />
+      <main id='main' className='site-main'>
+        <Route
+          render={({ location }) => (
+            <Switch location={location}>
+              <Route exact path='/' component={Home} />
+              <Route component={ErrorPage} />
+            </Switch>
+          )}
+        />
       </main>
       <Footer />
-    </Fragment>
+    </Router>
   );
 }
 
