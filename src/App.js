749806c5397e06bb2d09f1cb7558ac51c8357d41
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-07-12 20:47:40 -0700

    Add Bootstrap. Add initial markup.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 import Header from './components/main/Header';
 import Footer from './components/main/Footer';
+import './styles/index.css';
 
 function App() {
   return (
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ErrorPage extends Component {
   render() {
-    return <h1>ErrorPage</h1>;
+    return (
+      <div className='full-width-row error-primary-bg'>
+        <div className='error-div'>
+          <h2 className='error-div__header text-center '>404</h2>
+          <h2
+            className='error-div__sub-header text-center'
+            style={{ marginBottom: '10px' }}
+          >
+            Oh no! Page not found.
+          </h2>
+          <Link
+            to={`/`}
+            className='btn btn--white center-content'
+            onClick={this.displayNone}
+          >
+            Return to Home
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
