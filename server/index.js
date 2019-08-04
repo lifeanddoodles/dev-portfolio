bbc7fa38382e3162068307fdda10c3e5d850e932
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-08-03 19:19:30 -0700

    Initial SSR setup

---
+++ server/index.js
@@ -0,0 +1,60 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// require('../routes/projects')(app);
+// require('../routes/skills')(app);
+// require('../routes/calltoactions')(app);
+// require('../routes/send')(app);
+
+// if (process.env.NODE_ENV === 'production') {
+//   //Express will serve up production assets
+//   app.use(express.static('client/build'));
+
+//   //Express will serve up the index.html file
+//   //if it doesn't recognize the route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+
+import path from 'path';
+import fs from 'fs';
+
+import React from 'react';
+import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+
+import App from '../src/App';
+
+const PORT = process.env.PORT || 3006;
+const app = express();
+
+app.use(express.static('./build'));
+
+app.get('/*', (req, res) => {
+  const app = ReactDOMServer.renderToString(<App />);
+
+  const indexFile = path.resolve('./build/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    );
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`😎 Server is listening on port ${PORT}`);
+});
