49ad94831bd9a0a9678406e3990c84019e48002c
Sandra Vargas <sandra@Sandras-MacBook-Pro.local>
2019-09-12 14:15:17 -0700

    Update config file

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   ETHEREAL_EMAIL_PORT: 587,
   ETHEREAL_EMAIL_SECURE: false,
   PROD_EMAIL: 'sandravargasgro@gmail.com',
-  PROD_PASS: 'baka_365ml'
+  PROD_PASS: 'majika_52g-'
   // PROD_EMAIL: 'sandra@lifeanddoodles.com',
   // PROD_PASS: 'rock$fr33Lance',
   // PROD_EMAIL_SERVER: 'mail.lifeanddoodles.com',
